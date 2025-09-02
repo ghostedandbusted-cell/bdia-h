aa4edb8593a9b7ffb14ee72013194dc9f0cdcc62
ghostedandbusted-cell <ghostedandbusted@gmail.com>
2025-09-02 18:31:02 +0530

    Updated index.css

--- src/components/common/Navigation.jsx
+++ src/components/common/Navigation.jsx
@@ -24,29 +24,29 @@ const Navigation = () => {
   ]
 
   return (
-    <nav className='fixed top-0 left-0 right-0 z-50 bg-white/90 backdrop-blur-sm border-b border-gray-100'>
+    <nav className='fixed top-0 left-0 right-0 z-50 bg-white/95 backdrop-blur-md border-b border-gray-200'>
       <div className='container mx-auto lg:px-12 px-6'>
-        <div className='flex items-center justify-between h-16 lg:h-18'>
+        <div className='flex items-center justify-between h-16 lg:h-20'>
           {/* Logo/Brand */}
-          <Link to='/' className='font-[font2] text-xl lg:text-2xl uppercase text-black hover:text-[#D3FD50] transition-colors duration-300'>
+          <Link to='/' className='font-[font2] text-2xl lg:text-3xl uppercase text-black hover:text-[#D3FD50] transition-colors duration-300'>
             K72
           </Link>
 
-          {/* Desktop Navigation */}
-          <ul className='hidden lg:flex items-center space-x-6 xl:space-x-8'>
+          {/* Navigation Items */}
+          <ul className='hidden lg:flex items-center space-x-8'>
             {navItems.map((item, index) => (
               <li key={index}>
                 {item.link ? (
                   <Link 
                     to={item.link}
-                    className='font-[font1] text-sm xl:text-base text-black hover:text-[#D3FD50] transition-colors duration-300 uppercase tracking-wide'
+                    className='font-[font1] text-sm text-black hover:text-[#D3FD50] transition-colors duration-300 uppercase tracking-wide'
                   >
                     {item.name}
                   </Link>
                 ) : (
                   <button 
                     onClick={item.action}
-                    className='font-[font1] text-sm xl:text-base text-black hover:text-[#D3FD50] transition-colors duration-300 uppercase tracking-wide'
+                    className='font-[font1] text-sm text-black hover:text-[#D3FD50] transition-colors duration-300 uppercase tracking-wide'
                   >
                     {item.name}
                   </button>
@@ -55,9 +55,9 @@ const Navigation = () => {
             ))}
           </ul>
 
-          {/* Mobile Navigation - Horizontal Scroll */}
-          <div className='lg:hidden flex-1 ml-6'>
-            <div className='flex items-center space-x-4 overflow-x-auto scrollbar-hide'>
+          {/* Mobile Navigation - Simplified horizontal scroll */}
+          <div className='lg:hidden flex-1 ml-8'>
+            <div className='flex items-center space-x-6 overflow-x-auto scrollbar-hide'>
               {navItems.map((item, index) => (
                 <div key={index} className='flex-shrink-0'>
                   {item.link ? (
